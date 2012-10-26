5eec1ed0e56858a63342c982a8778a4ebb1d0663
Kyle Red <awesomistguy@gmail.com>
2012-10-26 18:56:01 +0200

    Added controllers

--- application/controllers/customers.php
+++ application/controllers/customers.php
@@ -1,12 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
-
+class Customers extends CI_Controller {
+	
 	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
 }
-
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
--- application/controllers/suppliers.php
+++ application/controllers/suppliers.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Suppliers extends CI_Controller {
 
 	public function index()
 	{
@@ -8,5 +8,3 @@ class Welcome extends CI_Controller {
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
